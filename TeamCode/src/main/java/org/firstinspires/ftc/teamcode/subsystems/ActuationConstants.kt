d46d7dcb6b5c00957015fe4ca063a6e750b8ec52
Armaan Priyadarshan <43357808+armaanpriyadarshan@users.noreply.github.com>
2023-02-23 11:08:37 -0500

    Update ActuationConstants.kt
    Intake changes

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ActuationConstants.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/ActuationConstants.kt
@@ -6,7 +6,7 @@ class ActuationConstants {
     @Config
     object LiftConstants {
         @JvmField var targetPosition = 0
-        val LIFT_POSITIONS = arrayListOf(70, 190, 330)
+        val LIFT_POSITIONS = arrayListOf(75, 190, 330)
     }
 
     @Config
@@ -18,16 +18,16 @@ class ActuationConstants {
 
     @Config
     object ExtensionConstants {
-        @JvmField var targetPosition = 0.42
-        const val RETRACTED = 0.475
-        const val TRANSFER = 0.482
+        @JvmField var targetPosition = 0.47
+        const val RETRACTED = 0.47
+        const val TRANSFER = 0.481
         const val EXTENDED = 0.53
     }
 
     @Config
     object ArmConstants {
         @JvmField var targetPosition = 0.525
-        const val IDLE = 0.531
+        const val IDLE = 0.53
         const val TRANSFER = 0.535
         const val DOWN = 0.456
         const val FIRST_JUNCTION = 0.495
@@ -36,6 +36,7 @@ class ActuationConstants {
     @Config
     object ClawConstants {
         @JvmField var targetPosition = 0.66
+        const val INIT = 0.72
         const val OPEN = 0.66
         const val CLOSED = 0.62
     }
